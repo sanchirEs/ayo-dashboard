94dc905616f75982dfbe6b1d3762e785b5df3fde
sanchirEs <sanchirenkhamgalan@gmail.com>
2025-08-10 16:29:23 +0800

    gh

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,1 @@
 export { handlers as GET, handlers as POST } from "@/auth";
-import { handlers } from "@/auth";
-
-export const { GET, POST } = handlers;
-// import NextAuth from "next-auth";
-// import authOptions from "@/auth.config";
-
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
